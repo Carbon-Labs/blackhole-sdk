0cf0a6c94c53e1f3b5526884478429b9a9bafb68
anas <anas.saber@handicap24.com>
2021-07-07 22:50:34 +0200

    remove prod from blackhole sdk

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = Object.freeze({
-    isTest: process.env.NODE_ENV !== "production",
+    isTest: true,
     blockchain: {
-        websocket: process.env.NODE_ENV === "production" ? "wss://api-ws.zilliqa.com" : "wss://dev-ws.zilliqa.com",
-        api: process.env.NODE_ENV === "production" ? "https://ssn.zillet.io" : "https://dev-api.zilliqa.com",
-        chainId: process.env.NODE_ENV === "production" ? 1 : 333,
+        websocket: "wss://dev-ws.zilliqa.com",
+        api: "https://dev-api.zilliqa.com",
+        chainId: 333,
         msgVersion: 1,
     },
     carbonToken: process.env.NODE_ENV === "production" ? "0x1720dcaef87686003f0532792450b03966543d6a" : "0x1720dcaef87686003f0532792450b03966543d6a",

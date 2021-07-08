766de87e79e232ce7202f2d254399f67f2d49100
anas <anas.saber@handicap24.com>
2021-07-08 10:20:18 +0200

    add select tokens

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
         msgVersion: 1,
     },
     carbonToken: process.env.NODE_ENV === "production" ? "0x1720dcaef87686003f0532792450b03966543d6a" : "0x1720dcaef87686003f0532792450b03966543d6a",
-    proxyContract: process.env.NODE_ENV === "production" ? "0x5c84c966f71ead492af204f9a16cb32e738d1fbb" : "0x5c84c966f71ead492af204f9a16cb32e738d1fbb",
+    proxyContract: process.env.NODE_ENV === "production" ? "0xebf46306adda6021228c236f9418a13f2d5a00ae" : "0xebf46306adda6021228c236f9418a13f2d5a00ae",
     files: {
         withdrawProvUrl: "https://siasky.net/AABR0XVHg7SDyVKNk0xqHnkojFIzharCC3QtpyTLHYojDg",
         withdrawVerificationUrl: "https://siasky.net/CADe3pSNzXpMp4ZaNeyZMefnKv1fmRIsfuyP8EYOWpPLAA",

5ff1e75f74c00754a98e114f685ec3789b38a2a1
anas <anas.saber@handicap24.com>
2021-07-09 15:39:15 +0200

    add new proxy contract

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
         msgVersion: 1,
     },
     carbonToken: process.env.NODE_ENV === "production" ? "0x1720dcaef87686003f0532792450b03966543d6a" : "0x1720dcaef87686003f0532792450b03966543d6a",
-    proxyContract: process.env.NODE_ENV === "production" ? "0x94ffe5e922a7d2481e351952d142b08b6450dbd0" : "0x94ffe5e922a7d2481e351952d142b08b6450dbd0",
+    proxyContract: process.env.NODE_ENV === "production" ? "0xde66d4846aa6a709ced6512551b1bbf689a16c9d" : "0xde66d4846aa6a709ced6512551b1bbf689a16c9d",
     files: {
         withdrawProvUrl: "https://siasky.net/AABR0XVHg7SDyVKNk0xqHnkojFIzharCC3QtpyTLHYojDg",
         withdrawVerificationUrl: "https://siasky.net/CADe3pSNzXpMp4ZaNeyZMefnKv1fmRIsfuyP8EYOWpPLAA",
